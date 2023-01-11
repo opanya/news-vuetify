c30c9d5f6b27d2cc8425358b22dfeabf9cc5a287
Andrey <golgus17@gmail.com>
2023-01-11 19:58:41 +0300

    create web page

--- news/src/App.vue
+++ news/src/App.vue
@@ -1,60 +1,52 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar color="blue darken" dark app>
+      <v-app-bar-nav-icon v-on:click="navigation = !navigation"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        Новости
+      </v-toolbar-title>
     </v-app-bar>
-
+    <v-navigation-drawer v-model="navigation" absolute temporary>
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld/>
+      <v-card>
+        <v-img src="https://picsum.photos/500" class="white--text align-end"
+          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="200px">
+          <v-card-title v-text="87464867"></v-card-title>
+        </v-img>
+      <v-card-text>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, praesentium iusto aliquam ullam similique provident totam optio consectetur quis? Quibusdam tempora debitis veniam id sed recusandae beatae minus vitae quod.
+        </p>
+      </v-card-text>
+      
+        <v-card-actions>
+          <v-spacer></v-spacer>
+      
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+      
+          <v-btn icon>
+            <v-icon>mdi-bookmark</v-icon>
+          </v-btn>
+      
+          <v-btn icon>
+            <v-icon>mdi-share-variant</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
   },
-
   data: () => ({
-    //
+    navigation: false
   }),
 };
 </script>
